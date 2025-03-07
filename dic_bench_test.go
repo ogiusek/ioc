330f29c1c9436d582aef597389992640587630bc
ogiusek <kowalewski.olgierd@gmail.com>
2025-03-07 14:50:05 +0100

    improved bench tests

--- dic_bench_test.go
+++ dic_bench_test.go
@@ -1,6 +1,7 @@
 package ioc_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/ogiusek/ioc"
@@ -8,62 +9,82 @@ import (
 
 // go test -bench=.
 
-var iterations int = 1000000
-
 func BenchmarkTransientRetreive(b *testing.B) {
 	initial := 1
 	c := ioc.NewContainer()
-	ioc.RegisterTransient(c, func(d ioc.Dic) *int {
-		service := initial
-		return &service
+	ioc.RegisterTransient(c, func(d ioc.Dic) int {
+		return initial
 	})
 
 	b.ResetTimer()
-	for i := 0; i <= iterations; i++ {
-		ioc.Get[*int](c)
+	for i := 0; i < b.N; i++ {
+		ioc.Get[int](c)
 	}
 }
 
 func BenchmarkScopedRetrieve(b *testing.B) {
 	initial := 1
 	c := ioc.NewContainer()
-	ioc.RegisterScoped(c, func(d ioc.Dic) *int {
-		service := initial
-		return &service
+	ioc.RegisterScoped(c, func(d ioc.Dic) int {
+		return initial
 	})
 	scope := ioc.Scope(c)
 
 	b.ResetTimer()
-	for i := 0; i <= iterations; i++ {
-		ioc.Get[*int](scope)
+	for i := 0; i < b.N; i++ {
+		ioc.Get[int](scope)
 	}
 }
 
 func BenchmarkScopeCreation(b *testing.B) {
 	initial := 1
 	c := ioc.NewContainer()
-	ioc.RegisterScoped(c, func(d ioc.Dic) *int {
-		service := initial
-		return &service
+	ioc.RegisterScoped(c, func(d ioc.Dic) int {
+		return initial
 	})
-	scope := ioc.Scope(c)
 
 	b.ResetTimer()
-	for i := 0; i <= iterations; i++ {
-		ioc.Get[*int](scope)
+	for i := 0; i < b.N; i++ {
+		ioc.Scope(c)
 	}
 }
 
 func BenchmarkSingletonRetrieve(b *testing.B) {
 	initial := 1
 	c := ioc.NewContainer()
-	ioc.RegisterSingleton(c, func(d ioc.Dic) *int {
-		service := initial
-		return &service
+	ioc.RegisterSingleton(c, func(d ioc.Dic) int {
+		return initial
 	})
 
 	b.ResetTimer()
-	for i := 0; i <= iterations; i++ {
-		ioc.Get[*int](c)
+	for i := 0; i < b.N; i++ {
+		ioc.Get[int](c)
+	}
+}
+
+func BenchmarkMapForComparison(b *testing.B) {
+	key := "item"
+	testedMap := map[string]int{
+		key: 1,
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = testedMap[key]
+	}
+}
+
+func BenchmarkMapWithMutexForComparison(b *testing.B) {
+	key := "item"
+	testedMap := map[string]int{
+		key: 1,
+	}
+	mutex := &sync.Mutex{}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mutex.Lock()
+		_ = testedMap[key]
+		mutex.Unlock()
 	}
 }
