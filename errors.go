351e94e2f188cba6e92535ee5343096d38b3e945
ogiusek <kowalewski.olgierd@gmail.com>
2026-04-18 16:29:22 +0200

    improved error logging

--- errors.go
+++ errors.go
@@ -3,10 +3,9 @@ package ioc
 import "errors"
 
 var (
-	ErrIsntPointer           error = errors.New("isn't a pointer")
-	ErrIsntPointerToStruct   error = errors.New("isn't a pointer to a struct")
-	ErrServiceIsntRegistered error = errors.New("service isn't registered")
+	ErrIsntPointer         error = errors.New("isn't a pointer")
+	ErrIsntPointerToStruct error = errors.New("isn't a pointer to a struct")
 
-	ErrCircularDependency error = errors.New("circular dependency")
-	ErrMissingDependency  error = errors.New("missing dependency")
+	ErrServiceIsntRegistered error = errors.New("service isn't registered")
+	ErrCircularDependency    error = errors.New("circular dependency")
 )
